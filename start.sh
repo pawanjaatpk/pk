dc16fdb1fb498dfb6946d193eb9220e05b5fb84a
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-02-08 00:25:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/pawanjaatpk/pk.git /pk
+git clone https://github.com/pawanjaatpk/pk.git/pk
 cd /pk
 pip3 install -r requirements.txt
 echo "Starting Bot....GreyMatter_Bots"
